3b14ccbb4119afff2e38dbb6f57048c4e72a4051
magdalenabaric <baric.magdalena2@gmail.com>
2023-11-03 17:32:25 +0100

    Promjena konzolne aplikacije

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Magdalene Barić");
+            Console.WriteLine("Pozdrav iz aplikacije od Magdalene Barić (0128673254)");
         }
     }
 }
